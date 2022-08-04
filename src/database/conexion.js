16a37ea754aca84cce92ef0d5ff4854adf27ae7a
DarioMarret <javier_dario_marret@hotmail.com>
2022-08-04 02:22:30 -0500

    primer commit_6

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import mysql from 'mysql2/promise';
 
-export const conexion = mysql.createPool(JSON.parse(fs.readFileSync(path.join(__dirname, '../config/database.json'), 'utf8')));
+export const conexion = mysql.createPool(JSON.parse(fs.readFileSync(path.join(__dirname, '../config/db.json'), 'utf8')));
 conexion.on(`error`, (err) => {
     console.error(`Connection error ${err.code}`);
 });
