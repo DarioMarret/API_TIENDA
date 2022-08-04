c6bbdd71aa8ce2866835387499a31b29a1586f5b
DarioMarret <javier_dario_marret@hotmail.com>
2022-08-04 02:29:37 -0500

    primer commit_7

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -12,7 +12,7 @@ exports.options = {
       url: "https://swagger.io",
       description: "Find more info here",
     },
-    host: "localhost:5055",
+    host: "https://rec.netbot.ec/v1",
     schemes: ["http"],
     consumes: ["application/json"],
     produces: ["application/json"],
