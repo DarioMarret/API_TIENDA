ed238db3f5ebea6ee123068d715f4d72fa299216
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-05 10:13:57 -0500

    sqlite

--- src/controller/tienderos.controller.js
+++ src/controller/tienderos.controller.js
@@ -119,6 +119,8 @@ export const UpdateTiendas = async (req, reply) => {
         const { id, nombre_tienda, responsable, cedula, comision, token_sistema, usuario, password, new_password, direccion, telefono } = req.body;
         let hash = await hashPassword(new_password)
         comision == null ? 0.50 : parseFloat(comision);
+        console.log(req.body)
+        console.log("\n")
         if(new_password != ""){
             const tienda = await conexion.query(`UPDATE tienderos_usuarios 
             SET nombre_tienda = ?, responsable = ?, cedula = ?, comision = ?, token_sistema = ?, direccion = ?, usuario = ?, telefono = ? WHERE id = ?`, 
